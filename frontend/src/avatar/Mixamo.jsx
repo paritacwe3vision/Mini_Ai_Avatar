4cca3e6f1c207c6a0c3bece7c26769ff23925dfc
Parita Chauhan <paritac.we3vision@gmail.com>
2026-08-03 10:05:37 +0530

    Create Mixamo.jsx

---
+++ frontend/src/avatar/Mixamo.jsx
@@ -0,0 +1,110 @@
+import { useRef, useEffect } from "react";
+import {
+  useGLTF,
+  useFBX,
+  useAnimations,
+} from "@react-three/drei";
+
+export default function Mixamo({ emotion = "Waving" }) {
+  const group = useRef();
+
+  // ==========================
+  // Avatar Model
+  // ==========================
+  const { scene } = useGLTF("/avatar/standing_waving.glb");
+
+  // ==========================
+  // Load Waving Animation
+  // ==========================
+  const waving = useFBX("/animations/Standing_waving.fbx");
+  const happy = useFBX("/animations/Happy.fbx");
+  const thinking = useFBX("/animations/Thinking.fbx");
+  const sad = useFBX("/animations/Sad.fbx");
+  const angry = useFBX("/animations/Angry.fbx");
+
+  // Rename animation
+  if (waving.animations.length > 0) {
+    waving.animations[0].name = "Waving";
+  }
+  if (happy.animations.length > 0) {
+  happy.animations[0].name = "Happy";
+  }
+
+  if (thinking.animations.length > 0) {
+    thinking.animations[0].name = "Thinking";
+  }
+
+  if (sad.animations.length > 0) {
+    sad.animations[0].name = "Sad";
+  }
+
+  if (angry.animations.length > 0) {
+    angry.animations[0].name = "Angry";
+  }
+
+  // Attach animation to avatar
+const { actions } = useAnimations(
+  [
+    waving.animations[0],
+    happy.animations[0],
+    thinking.animations[0],
+    sad.animations[0],
+    angry.animations[0],
+  ],
+  group
+);
+
+  // ==========================
+  // Enable Shadows
+  // ==========================
+  useEffect(() => {
+    scene.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+  }, [scene]);
+
+  // ==========================
+  // Play Animation
+  // ==========================
+ useEffect(() => {
+
+  if (!actions) return;
+
+  Object.values(actions).forEach((action)=>{
+    action.stop();
+  });
+
+  const action = actions[emotion];
+
+  if (!action) {
+    console.log("Animation not found:", emotion);
+    return;
+  }
+
+  action.reset();
+  action.fadeIn(0.3);
+  action.play();
+
+
+}, [actions, emotion]);
+
+  // ==========================
+  // Render Avatar
+  // ==========================
+  return (
+    <group
+      ref={group}
+      position={[0, 0.5, 0]}
+      rotation={[ -Math.PI/2,0, 0]}
+      scale={[0.7, 0.7, 0.7]}
+    >
+      <primitive object={scene} />
+    </group>
+  );
+}
+
+// Preload Assets
+useGLTF.preload("/avatar/standing_waving.glb");

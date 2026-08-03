517c0811e8c027431dd2f88c9de30a8dda11f85e
Parita Chauhan <paritac.we3vision@gmail.com>
2026-08-03 09:46:25 +0530

    enhanced the avatar
    enhanced the avatar by making her standing and wave hand properly.

--- frontend/src/avatar/Scene.jsx
+++ frontend/src/avatar/Scene.jsx
@@ -4,50 +4,60 @@ import { OrbitControls, Environment } from "@react-three/drei";
 
 import ReadyPlayerAvatar from "./ReadyPlayerAvatar";
 
-
 export default function Scene() {
-
   return (
     <Canvas
+      shadows
       camera={{
-        position: [0, 1.5, 5],
-        fov: 45,
+        position: [0, 1.3, 3.5],
+        fov: 35,
       }}
       style={{
         width: "100%",
         height: "100%",
       }}
     >
-
       <Suspense fallback={null}>
 
-        {/* Lights */}
-        <ambientLight intensity={1.5} />
+        {/* Lighting */}
+        <ambientLight intensity={0.8} />
 
         <directionalLight
-          position={[3, 5, 3]}
+          position={[5, 8, 5]}
           intensity={2}
           castShadow
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
         />
+
+        <directionalLight
+          position={[-5, 4, 2]}
+          intensity={0.8}
+        />
+
+        {/* Studio Environment */}
+        <Environment preset="studio" />
 
 
         {/* Avatar */}
         <ReadyPlayerAvatar />
 
 
-        {/* Environment */}
-        <Environment preset="studio" />
-
-
-        {/* Camera Control */}
+        {/* Camera Controls */}
         <OrbitControls
-          target={[0, 1, 0]}
+          target={[0, 1.2, 0]}
+          enablePan={false}
           enableZoom={true}
+
+          minDistance={2.5}
+          maxDistance={5}
+
+          // allow small vertical movement
+          minPolarAngle={Math.PI / 3}
+          maxPolarAngle={Math.PI / 1.8}
         />
 
-
       </Suspense>
-
     </Canvas>
   );
 }
